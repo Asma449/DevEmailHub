dfd937810751e2b59bcaebfc825fe1db9e5f700e
asma449 <asmamughal2391@gmail.com>
2026-02-01 20:21:06 +0500

    Initial commit - DevEmailHub project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,53 @@
+import "boxicons/css/boxicons.min.css";
+import Spline from "@splinetool/react-spline";
+const Hero = () => {
+  return (
+    <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(100vh-6rem)]">
+      <div
+  data-aos="fade-right"
+  data-aos-offset="300"
+  data-aos-easing="ease-in-sine"
+  className="max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0"
+>
+        {/* Tag */}
+        <div className="relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full">
+          <div className="absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1 text-sm">
+            <i className="bx bx-diamonds"></i>
+            INTRODUCING
+          </div>
+        </div>
+
+        <h1 className="text-4xl font-bold mt-6">
+          EMAIL FOR <br /> DEVELOPERS
+        </h1>
+
+        <p className="text-base sm:text-lg tracking-wider text-gray-400 max-w-[25rem] lg:max-w-[30rem] mt-4">
+          THE BEST WAY TO REACH HUMANS INSTEAD OF SPAM FOLDERS, DEVELOP
+          TRANSACTIONAL AND MARKETING EMAIL AT SCALE.
+        </p>
+
+        <div className="flex gap-4 mt-12">
+          <a
+            className="border border-[#2a2a2a] py-2 sm:py-3 px-4 sm:px-5 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 hover:bg-[#1a1a1a]"
+            href="#"
+          >
+            Documentation <i className="bx bx-link-external"></i>
+          </a>
+
+          <a
+            className="border border-[#2a2a2a] py-2 sm:py-3 px-8 sm:px-10 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 hover:bg-[#1a1a1a] bg-gray-300 text-black hover:text-white"
+            href="#"
+          >
+            GetStarted <i className="bx bx-link-external"></i>
+          </a>
+        </div>
+      </div>
+
+         <Spline className="absolute lg:top-0 top-[-20%] bottom=0  lg:left-[25%] sm:left-[-2%] h-full" scene="https://prod.spline.design/K879xwx5v2pAR74D/scene.splinecode" />
+
+
+    </main>
+  );
+};
+
+export default Hero;
